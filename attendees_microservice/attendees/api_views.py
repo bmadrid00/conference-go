702bacc4860c58c7432b1298790dfd54d6bedcc6
Bradley Madrid <bradley.madrid2@gmail.com>
2023-03-28 22:16:31 -0500

    Convert attendee api to microservice

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,13 +1,16 @@
 from django.http import JsonResponse
-from .encoders import AttendeeListEncoder, AttendeeDetailEncoder
-from .models import Attendee
-from events.models import Conference
+from .encoders import (
+    AttendeeListEncoder,
+    AttendeeDetailEncoder,
+    ConferenceVODetailEncoder,
+)
+from .models import Attendee, ConferenceVO
 import json
 from django.views.decorators.http import require_http_methods
 
 
 @require_http_methods(["POST", "GET"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id):
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -28,14 +31,15 @@ def api_list_attendees(request, conference_id):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(attendees, encoder=AttendeeListEncoder, safe=False)
     else:
         content = json.loads(request.body)
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f"/api/conferences/{conference_vo_id}/"
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
@@ -82,9 +86,9 @@ def api_show_attendee(request, id):
     else:
         content = json.loads(request.body)
         try:
-            conference = Conference.objects.get(id=content["conference"])
+            conference = ConferenceVO.objects.get(id=content["conference"])
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
---
+++ monolith/common/json.py
@@ -0,0 +1,42 @@
+from json import JSONEncoder
+from datetime import datetime
+from django.db.models import QuerySet
+
+
+class DateEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return datetime.isoformat(o)
+        else:
+            return super().default(o)
+
+
+class QuerySetEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, QuerySet):
+            return list(o)
+        else:
+            return super().default(o)
+
+
+class ModelEncoder(DateEncoder, QuerySetEncoder, JSONEncoder):
+    encoders = {}
+
+    def default(self, o):
+        if isinstance(o, self.model):
+            d = {}
+            if hasattr(o, "get_api_url"):
+                d["href"] = o.get_api_url()
+            for propert in self.properties:
+                attr = getattr(o, propert)
+                if propert in self.encoders:
+                    encoder = self.encoders[propert]
+                    attr = encoder.default(attr)
+                d[propert] = attr
+            d.update(self.get_extra_data(o))
+            return d
+        else:
+            return super().default(o)
+
+    def get_extra_data(self, o):
+        return {}
